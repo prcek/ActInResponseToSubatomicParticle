a6292099d134756b01cf524b49c18351745f5175
Tomas Hluchan <tomas.hluchan@gmail.com>
2017-08-17 12:40:33 +0200

    tree

--- src/CfgPanel.js
+++ src/CfgPanel.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Input from 'material-ui/Input/Input';
-import InputLabel from 'material-ui/Input/InputLabel';
+//import Input from 'material-ui/Input/Input';
+//import InputLabel from 'material-ui/Input/InputLabel';
+import TextField from 'material-ui/TextField'
 import Button from 'material-ui/Button'
 import Grid from 'material-ui/Grid';
 
-import FormControlLabel from 'material-ui/Form/FormControlLabel';
-import FormControl from 'material-ui/Form/FormControl';
-import FormLabel from 'material-ui/Form/FormLabel';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+
+
+
+const styleSheet = createStyleSheet(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+}));
+
 
 class CfgPanel extends Component {
 
@@ -15,13 +29,9 @@ class CfgPanel extends Component {
         super(props);
         this.state = {name: this.props.cfg.name, surname: this.props.cfg.surname};
 
-        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value});
-    }
 
     handleSubmit(event) {
         alert('A name was submitted: ' + this.state.name + ' ' + this.state.surname );
@@ -32,30 +42,41 @@ class CfgPanel extends Component {
 
 
     render() {
+        const classes = this.props.classes;
         return (
-            <form onSubmit={this.handleSubmit}>
-                    <span>
-                    <FormControl margin="normal" required="true" error="true">
-                        <InputLabel>
-                            Name
-                        </InputLabel>
-                        <Input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
-                    </FormControl>
-                    </span>
-                    <FormControl margin="normal">
-                        <InputLabel>
-                        Surname
-                        </InputLabel>
-                        <Input type="text" name="surname" value={this.state.surname} onChange={this.handleChange} />
-                    </FormControl>
-                    <Button  type="submit"> Save </Button>
-            </form>
+            <Grid container direction="row" align="baseline">
+                <Grid item>
+                    <div className={classes.container}>
+                        <TextField
+                            id="name"
+                            label="Name"
+                            className={classes.textField}
+                            value={this.state.name}
+                            onChange={event => this.setState({ name: event.target.value })}
+                            margin="normal"
+                        />
+                        <TextField
+                            id="surname"
+                            label="Surname"
+                            className={classes.textField}
+                            value={this.state.surname}
+                            onChange={event => this.setState({ surname: event.target.value })}
+                            margin="normal"
+                        />
+                    </div>
+                </Grid>
+                <Grid item>
+                    <Button raised> Save </Button>
+                </Grid>
+            </Grid>
         )
     }
 }
 
+
 CfgPanel.propTypes = {
+    classes: PropTypes.object.isRequired,
     cfg: PropTypes.object.isRequired
 };
 
-export default CfgPanel;
+export default withStyles(styleSheet)(CfgPanel);
--- src/Tree.js
+++ src/Tree.js
@@ -1,44 +1,64 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import FolderIcon from 'material-ui-icons/Folder';
 
-class TreeNode extends Component {
-    constructor(props) {
-        super(props);
-    }
+const styleSheet = createStyleSheet(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    background: theme.palette.background.paper,
+  },
+}));
 
-    renderSelf() {
-        return (
-            <div> {this.props.data.name}, level {this.props.level} </div>
-        )
-    }
 
-    renderChild(n,l) {
-        return (
-            <li> <TreeNode data={n} level={l}/> </li>
-        )
+function flatIt(nodes,level=0,parent) {
+    var childs = []
+    for(var ul of nodes.nodes) {
+        childs.push(...flatIt(ul,level+1,nodes))
     }
-    
-    render() {
-        const me = this.renderSelf();
-        const nodes  = this.props.data.nodes.map((n)=>this.renderChild(n,this.props.level+1));
-        return (
-            <div> {me} <ul>{nodes}</ul> </div>
-        )
+    const isLeaf = (childs.length === 0)
+    if (parent) {
+        return [{ id: nodes.id, name: nodes.name, isLeaf: isLeaf, level:level, parentId:parent.id}, ...childs]
+    } else {
+        return [{ id: nodes.id, name: nodes.name, isLeaf: isLeaf, level:level, parentId:null}, ...childs]
     }
-
 }
 
 class Tree extends Component {
     constructor(props) {
         super(props);
+        this.flat = flatIt(this.props.data);
+    }
+
+    renderItem(n) {
+        return (
+            <ListItem key={n.id} button={n.isLeaf} style={{paddingLeft: (n.level*32)+'px'}}>
+                 <ListItemIcon>
+                    <FolderIcon />
+                </ListItemIcon>
+                <ListItemText primary={n.name}/>
+            </ListItem>
+        );
     }
 
     render() {
+        const classes = this.props.classes;
+        const nodes  = this.flat.map((n)=>this.renderItem(n));
         return (
-            <TreeNode data={this.props.data} level={0}/>
+            <div className={classes.root}>
+                <List >
+                    {nodes}
+                </List>
+            </div>
         )
     }
 }
 
 
-export default Tree;
+Tree.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styleSheet)(Tree);
