b152c71c17a53b3f401e9a9a7e7abfe982da186e
Tomas Hluchan <tomas.hluchan@gmail.com>
2017-08-18 10:23:32 +0200

    tree checkbox

--- src/Tree.js
+++ src/Tree.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
-import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import List, { ListItem, ListItemIcon, ListItemText, ListItemSecondaryAction } from 'material-ui/List';
 import FolderIcon from 'material-ui-icons/Folder';
+import Checkbox from 'material-ui/Checkbox';
 
 const styleSheet = createStyleSheet(theme => ({
   root: {
     width: '100%',
-    maxWidth: 360,
+    maxWidth: 560,
     background: theme.palette.background.paper,
   },
 }));
@@ -27,18 +28,67 @@ function flatIt(nodes,level=0,parent) {
 }
 
 class Tree extends Component {
+
+    state = {
+        checked: [],
+        toggled: []
+    }
     constructor(props) {
         super(props);
         this.flat = flatIt(this.props.data);
     }
 
+    handleToggle(event,n) {
+        console.log("handleToggle",n.id);
+
+        const { toggled } = this.state;
+        const currentIndex = toggled.indexOf(n.id);
+        const newToggled = [...toggled];
+
+        if (currentIndex === -1) {
+            newToggled.push(n.id);
+        } else {
+            newToggled.splice(currentIndex, 1);
+        }
+
+        this.setState({
+            toggled: newToggled,
+        });
+    }
+
+    handleCheck(event,n) {
+        console.log("handleCheck",n.id);
+
+        const { checked } = this.state;
+        const currentIndex = checked.indexOf(n.id);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(n.id);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        this.setState({
+            checked: newChecked,
+        });
+
+    }
+
     renderItem(n) {
         return (
-            <ListItem key={n.id} button={n.isLeaf} style={{paddingLeft: (n.level*32)+'px'}}>
+            <ListItem key={n.id} button dense onClick={event => this.handleToggle(event, n)} style={{paddingLeft: (n.level*32)+'px'}}>
                  <ListItemIcon>
                     <FolderIcon />
                 </ListItemIcon>
-                <ListItemText primary={n.name}/>
+                <ListItemText primary={n.name} secondary={n.id}/>
+                <ListItemSecondaryAction>
+                    <Checkbox
+                        onClick={event => this.handleCheck(event, n)}
+                        checked={this.state.checked.indexOf(n.id) !== -1}
+                        tabIndex="-1"
+                    />
+                </ListItemSecondaryAction>
             </ListItem>
         );
     }
